c6fb25b297df8b1d7d61234e3676fc95cd4fc873
justinph2013 <justinph2013@gmail.com>
2019-12-17 14:23:04 -0800

    Character manager

--- src/CharacterManager.java
+++ src/CharacterManager.java
@@ -1,8 +1,18 @@
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
-
-public class CharacterManager {
+import java.applet.*;
+import java.awt.event.KeyListener;
+public class CharacterManager implements KeyListener {
     private ArrayList<Character> characters;
     private final int MAX_CHARACTERS = 5;
+    Image[] imgs = new Images[IMAGE_FILES.length];
+
+    private static final String[] IMAGE_FILES = {
+            "bautista.jpg", "paley.jpg", "lorey.jpg", "dunbar.jpg", "gleason.jpg",
+            "OC.jpg", "deggeler.jpg", "lyons.jpg", "linsdell.jpg", ""
+    };
+
+
     public CharacterManager(){
 
     }
@@ -14,5 +24,27 @@ public class CharacterManager {
             characters.add(character);
         }
     }
+    public void initImages() {
+        for (int i = 0; i < images.length; i++) {
+            images[i] = getImage(getDocumentBase(), "resources/" + IMAGE_FILES[i]);
+        }
+    }
+    public Image getImage(int i) {
+        return Image[i];
+    }
 
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
 }
